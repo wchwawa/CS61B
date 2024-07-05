0cf93dafc4322a3c28e6400ed1c682ac27f6d4d2
wchwawa <wch19961116@gmail.com>
2024-07-05 12:27:48 +0800

    Proj1b: implement resize up and down, left getRecursion only.

--- proj1b/src/ArrayDeque61B.java
+++ proj1b/src/ArrayDeque61B.java
@@ -1,2 +1,138 @@
-package PACKAGE_NAME;public class ArrayDeque61B {
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
+
+public class ArrayDeque61B<T> implements Deque61B<T> {
+    private T[] items;
+    private int size;
+    private int itemLength;
+    public int nextFirst;
+    public int nextLast;
+
+    public ArrayDeque61B(){
+        items = (T[]) new Object[8];
+        size = 0;
+        itemLength = items.length;
+        this.nextFirst = items.length - 1;
+        this.nextLast = 0;
+
+    }
+
+    @Override
+    public void addFirst(T x) {
+        if (size == itemLength){
+            resize(2);
+        }
+        size ++;
+        items[nextFirst] = x;
+        nextFirst = Math.floorMod(nextFirst - 1, itemLength);
+
+    }
+
+    @Override
+    public void addLast(T x) {
+        if (size == itemLength){
+            resize(2);
+        }
+        size++;
+        items[nextLast] = x;
+        nextLast = Math.floorMod(nextLast + 1, itemLength);
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> l = new ArrayList<>();
+
+        for (int i = 0; i < items.length; i ++){
+            if (items[i] == null){
+                continue;
+            }else {
+                l.add(items[i]);
+            }
+        }
+        return l;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (size == 0){
+            return null;
+        }
+        if (size < 0.25 * itemLength){
+            resize(0.50);
+        }
+        size --;
+        T removedItem = items[nextFirst + 1];
+        items[nextFirst + 1] = null;
+        nextFirst = Math.floorMod(nextFirst + 1, itemLength);
+        return removedItem;
+    }
+
+    @Override
+    public T removeLast() {
+        if (size == 0){
+            return null;
+        }
+        if (size < 0.25 * itemLength){
+            resize(0.50);
+        }
+
+        size --;
+        T removedItem = items[nextLast - 1];
+        items[nextLast - 1] = null;
+        nextLast = Math.floorMod(nextLast - 1, itemLength);
+        return removedItem;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < itemLength && index >= 0){
+            return items[index];
+        }
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Math.floorMod(-1, 7));
+        System.out.println();
+    }
+
+    public void resize(double factor){
+        double cal = itemLength * factor;
+        int newLength = (int)cal;
+        T[] newItem = (T[]) new Object[newLength];
+
+        for (int i = 0; i < nextLast; i++) {
+            newItem[i] = items[i];
+        }
+        int totalFirstItemCount = itemLength - 1 - nextFirst;
+        int newItemLength = newItem.length;
+        for (int j = itemLength - 1; j > nextFirst; j --){
+            newItem[newItemLength - 1] = items[j];
+            newItemLength --;
+        }
+        items = newItem;
+        itemLength = items.length;
+        nextFirst = itemLength - totalFirstItemCount - 1;
+    }
+
 }
--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -1,4 +1,5 @@
 import jh61b.utils.Reflection;
+import org.checkerframework.checker.index.qual.PolyUpperBound;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,16 +9,176 @@ import java.util.List;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
-public class ArrayDeque61BTest {
+public class ArrayDeque61BTest  {
 
-//     @Test
-//     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
-//     void noNonTrivialFields() {
-//         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
-//                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-//                 .toList();
-//
-//         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-//     }
+     @Test
+     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
+     void noNonTrivialFields() {
+         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
+                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                 .toList();
+
+         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+     }
+
+     @Test
+    public void testAddFirstBasic(){
+         Deque61B<Integer> deque = new ArrayDeque61B<>();
+         deque.addFirst(1);
+         assertThat(deque.toList()).containsExactly(1);
+    }
+
+    @Test
+    public void testAddLastBasic(){
+        Deque61B<Integer> deque = new ArrayDeque61B<>();
+        deque.addLast(1);
+        assertThat(deque.toList()).containsExactly(1);
+    }
+
+    @Test
+    public void testAddLastAndFirst(){
+        Deque61B<Character> deque = new ArrayDeque61B<>();
+        deque.addLast('a');
+        deque.addFirst('b');
+        deque.addFirst('c');
+        deque.addLast('d');
+        deque.addFirst('e');
+        assertThat(deque.toList()).containsExactly('a', 'd','e','c','b').inOrder();
+    }
+
+/* Test GET INDEX */
+    @Test
+    public void testGet(){
+        Deque61B<Character> deque = new ArrayDeque61B<>();
+        deque.addLast('a');
+        deque.addFirst('b');
+        deque.addFirst('c');
+        deque.addLast('d');
+        deque.addFirst('e');
+        char gotItem = deque.get(5);
+        assertThat(gotItem).isEqualTo('e');
+    }
+
+    @Test
+    public void testIsEmptyPositive(){
+        Deque61B<Character> deque = new ArrayDeque61B<>();
+        assertThat(deque.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void testIsEmptyNegative(){
+        Deque61B<Character> deque = new ArrayDeque61B<>();
+        deque.addLast('a');
+        assertThat(deque.isEmpty()).isFalse();
+    }
+
+    /* TEST SIZE*/
+    @Test
+    public void testSize(){
+        Deque61B<Character> deque = new ArrayDeque61B<>();
+        deque.addLast('a');
+        assertThat(deque.size()).isEqualTo(1);
+        deque.addFirst('c');
+        deque.addLast('d');
+        deque.addFirst('e');
+        assertThat(deque.size()).isEqualTo(4);
+    }
+
+    @Test
+    public void testEmptySize(){
+        Deque61B<Character> deque = new ArrayDeque61B<>();
+        assertThat(deque.size()).isEqualTo(0);
+    }
+
+    /*test REMOVE*/
+    @Test
+    public void testRemoveFirst(){
+        Deque61B<Character> deque = new ArrayDeque61B<>();
+        deque.addLast('a');
+        deque.addFirst('b');
+        deque.addFirst('c');
+        deque.addLast('d');
+        deque.addFirst('e');
+        deque.removeFirst();
+        assertThat(deque.toList()).containsExactly('a', 'd', 'c','b').inOrder();
+    }
+
+    @Test
+    public void testRemoveLast(){
+        Deque61B<Character> deque = new ArrayDeque61B<>();
+        deque.addLast('a');
+        deque.addFirst('b');
+        deque.addFirst('c');
+        deque.addLast('d');
+        deque.addFirst('e');
+        deque.removeLast();
+        assertThat(deque.toList()).containsExactly('a', 'e', 'c','b').inOrder();
+    }
+
+    /*IMPORTANT: TEST RESIZE UP*/
+    @Test
+    public void testResizeUp(){
+        Deque61B<Character> deque = new ArrayDeque61B<>();
+        deque.addLast('a');
+        deque.addFirst('b');
+        deque.addFirst('c');
+        deque.addLast('d');
+        deque.addFirst('e');
+        deque.addFirst('g');
+        deque.addFirst('f');
+        deque.addFirst('p');
+        deque.addFirst('m');
+        deque.addFirst('e');
+        deque.addFirst('g');
+        deque.addFirst('f');
+        deque.addFirst('p');
+        deque.addFirst('m');
+        deque.addFirst('e');
+        deque.addFirst('g');
+        deque.addFirst('f');
+        deque.addFirst('p');
+        deque.addFirst('m');
+        assertThat(deque.size()).isEqualTo(19);
+    }
+
+    @Test
+    public void testResizeDown(){
+        Deque61B<Character> deque = new ArrayDeque61B<>();
+        deque.addLast('a');
+        deque.addFirst('b');
+        deque.addFirst('c');
+        deque.addLast('d');
+        deque.addFirst('e');
+        deque.addFirst('g');
+        deque.addFirst('f');
+        deque.addFirst('p');
+        deque.addFirst('m');
+        deque.addFirst('e');
+        deque.addFirst('g');
+        deque.addFirst('f');
+        deque.addFirst('p');
+        deque.addFirst('m');
+        deque.addFirst('e');
+        deque.addFirst('g');
+        deque.addFirst('f');
+        deque.addFirst('p');
+        deque.addFirst('m');
+        deque.removeFirst();
+        deque.removeLast();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        assertThat(deque.size()).isEqualTo(12);
+    }
+
 
 }
